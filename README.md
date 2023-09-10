300f48082c65097796ee605747a76f82a8d345c1
shuminchang <magic2313@gmail.com>
2023-09-11 06:21:41 +0800

    user registration, crispy form update

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # django_project
 Tutorial
-https://www.youtube.com/playlist?list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc
+https://www.youtube.com/playlist?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p
 
 Diagram
 https://app.diagrams.net/#G1kqKytRlFKWli2jkd4J6m9rY-qe4x9lU6
+
+Update
+Crispy Forms
+https://stackoverflow.com/questions/75495403/django-returns-templatedoesnotexist-when-using-crispy-forms
--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
+    "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -112,7 +115,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
